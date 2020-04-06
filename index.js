d49f0c84cf6e5f68ac65fd465ba0e26b0ff6ad48
arttujantunen1 <arttu.jan@gmail.com>
2020-04-06 12:20:28 +0300

    testi

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+const handler = (req, res) => {
+    console.log('server received request');
+    res.end('Hello World');
+};
+
+const server = http.createServer(handler);
+
+server.listen(port, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Server listening on port: ${port}`);
+    }
+});
