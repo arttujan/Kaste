3d069d6d8861cb7a7e8a0b8d15db9f4665842d51
arttujantunen1 <arttu.jan@gmail.com>
2020-04-06 12:57:47 +0300

    testi3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log('server received request');
-    res.end('Testi2');
+    res.end('Testi3');
 };
 
 const server = http.createServer(handler);
