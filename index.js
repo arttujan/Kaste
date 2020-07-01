225ca84b0013d641147b5b7210921e3a2f651dc2
arttujantunen1 <arttu.jan@gmail.com>
2020-07-01 14:23:34 +0300

    html testi 4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 const port = process.env.PORT || 3000;
 
 function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'test/html'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
         fs.readFile('./index.html', null, function(error, data) {
         if (error) {
             response.writeHead(404);
