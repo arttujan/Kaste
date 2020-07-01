99c0660ae264d1e2326d986a091dd75e4e714592
arttujantunen1 <arttu.jan@gmail.com>
2020-07-01 19:58:23 +0300

    index js testi

--- index.js
+++ index.js
@@ -2,20 +2,166 @@ const http = require("http");
 var fs = require('fs');
 const port = process.env.PORT || 3000;
 
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'application/content-stream'});
-        fs.readFile('./index.html', null, function(error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-}
 
-const server = http.createServer(onRequest);
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+//step 1) require the modules we need
+var
+http = require('http'),
+path = require('path'),
+fs = require('fs'),
+ 
+//these are the only file types we will support for now
+extensions = {
+	".html" : "text/html",
+	".css" : "text/css",
+	".js" : "application/javascript",
+	".png" : "image/png",
+	".gif" : "image/gif",
+	".jpg" : "image/jpeg"
+};
+ 
+//helper function handles file verification
+function getFile(filePath,res,page404,mimeType){
+	//does the requested file exist?
+	fs.exists(filePath,function(exists){
+		//if it does...
+		if(exists){
+			//read the fiule, run the anonymous function
+			fs.readFile(filePath,function(err,contents){
+				if(!err){
+					//if there was no error
+					//send the contents with the default 200/ok header
+					res.writeHead(200,{
+						"Content-type" : mimeType,
+						"Content-Length" : contents.length
+					});
+					res.end(contents);
+				} else {
+					//for our own troubleshooting
+					console.dir(err);
+				};
+			});
+		} else {
+			//if the requested file was not found
+			//serve-up our custom 404 page
+			fs.readFile(page404,function(err,contents){
+				//if there was no error
+				if(!err){
+					//send the contents with a 404/not found header 
+					res.writeHead(404, {'Content-Type': 'text/html'});
+					res.end(contents);
+				} else {
+					//for our own troubleshooting
+					console.dir(err);
+				};
+			});
+		};
+	});
+};
+ 
+//a helper function to handle HTTP requests
+function requestHandler(req, res) {
+	var
+	fileName = path.basename(req.url) || 'index.html',
+	ext = path.extname(fileName),
+	localFolder = __dirname + '/public/',
+	page404 = localFolder + '404.html';
+ 
+	//do we support the requested file type?
+	if(!extensions[ext]){
+		//for now just send a 404 and a short message
+		res.writeHead(404, {'Content-Type': 'text/html'});
+		res.end("&lt;html&gt;&lt;head&gt;&lt;/head&gt;&lt;body&gt;The requested file type is not supported&lt;/body&gt;&lt;/html&gt;");
+	};
+ 
+	//call our helper function
+	//pass in the path to the file we want,
+	//the response object, and the 404 page path
+	//in case the requestd file is not found
+	getFile((localFolder + fileName),res,page404,extensions[ext]);
+};
+ 
+
+const server = http.createServer(requestHandler);
 
 server.listen(port, err => {
     if (err) {
