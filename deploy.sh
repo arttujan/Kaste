bf01c3da17f70e39f37bad0e21e363f2682372b5
arttujantunen1 <arttu.jan@gmail.com>
2020-07-03 15:32:03 +0300

    fiksiyritys 2

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,31 @@
-# Deployment  
-# ----------  
+##################################################################################################################################
+# Deployment
+# ----------
 
-echo Handling node.js gulp deployment.  
+echo Handling node.js deployment.
 
-# 1. Select node version  
-selectNodeVersion  
+# 1. Select node version
+selectNodeVersion
 
-# 2. Install npm packages  
-if [ -e "$DEPLOYMENT_SOURCE./package.json" ]; then  
-  eval $NPM_CMD install  
-  exitWithMessageOnError "npm failed"  
-fi  
+# 2. Install npm packages
+if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
+  cd "$DEPLOYMENT_SOURCE"
+  eval $NPM_CMD install --production
+  exitWithMessageOnError "npm failed"
+  cd - > /dev/null
+fi
 
-# 3. Install bower packages  
-if [ -e "$DEPLOYMENT_SOURCE./bower.json" ]; then  
-  eval $NPM_CMD install bower  
-  exitWithMessageOnError "installing bower failed"  
-  ./node_modules/.bin/bower install  
-  exitWithMessageOnError "bower failed"  
-fi  
+# 3. Run gulp tasks
+if [ -e "$DEPLOYMENT_SOURCE/gulpfile.js" ]; then
+  echo "Running gulp tasks"
+  cd "$DEPLOYMENT_SOURCE"
+  eval './node_modules/.bin/gulp'
+  exitWithMessageOnError "gulp failed"
+  cd - > /dev/null
+fi
 
-# 4. Run gulp for build
-if [ -e "$DEPLOYMENT_SOURCE./gulpfile.js" ]; then  
-  eval $NPM_CMD install gulp 
-  exitWithMessageOnError "installing gulpfailed"  
-  ./node_modules/.bin/gulp serve
-  exitWithMessageOnError "gulp failed"  
-fi  
-
-# 5. KuduSync to Target  
-"$KUDU_SYNC_CMD" -v 500 -f "$DEPLOYMENT_SOURCE/dist" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"  
-exitWithMessageOnError "Kudu Sync to Target failed"
+# 4. KuduSync
+if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
+  "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh;node_modules;package.json;gulpfile.js;.gitignore;.htaccess"
+  exitWithMessageOnError "Kudu Sync failed"
+fi
