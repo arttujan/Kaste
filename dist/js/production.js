6bbe64941143dd3553c7bd41662a409ab5761db6
arttujantunen1 <arttu.jan@gmail.com>
2020-08-12 12:36:32 +0300

    azure ad rekennuksen aloitus, javascript filujen teko

--- dist/js/production.js
+++ dist/js/production.js
@@ -14,3 +14,19 @@
 function changeClass(){
     document.getElementById("soitin").className = "container visible";
 }
+// Config object to be passed to Msal on creation
+const msalConfig = {
+    auth: {
+      clientId: "c666fc28-6a50-4e9e-bf88-fc4bb4138e4c",
+      authority: "b6d5681b-4a40-4d3a-8e7b-03a70d3991b6",
+      redirectUri: "https://www.kaste.app",
+    },
+    cache: {
+      cacheLocation: "sessionStorage", // This configures where your cache will be stored
+      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    }
+};
+// Add here the endpoints for MS Graph API services you would like to use.
+const graphConfig = {
+    graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
+};
---
+++ src/js/graphConfig.js
@@ -0,0 +1,4 @@
+  // Add here the endpoints for MS Graph API services you would like to use.
+  const graphConfig = {
+    graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
+  };
