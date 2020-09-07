d9a591db89a445afe43f15772601511f6a64792a
arttujantunen1 <arttu.jan@gmail.com>
2020-09-07 13:14:15 +0300

    Skaalaus korjattu

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,3 @@
-!function(){
-    window;const e=document.documentElement;
-    if(e.classList.remove("no-js"),
-    e.classList.add("js"),
-    document.body.classList.contains("has-animations")) {
-        (window.sr=ScrollReveal()).reveal(".reveal-on-scroll", {
-            duration:600,distance:"20px",
-            easing:"cubic-bezier(0.5, -0.01, 0, 1.005)",
-            origin:"left",
-        interval:100
-    })
-}
-}();
 videojs.getPlayer('myPlayerID').ready(function () {
     // +++ Initialize variables and check for mobile device +++
     var player = this,
