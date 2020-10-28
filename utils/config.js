9b7a0d17547b0193831a47acdb15ca3880c17052
Antti Nikku <antti.nikku@icloud.com>
2020-10-28 16:32:01 +0200

    - Transmuxing HLS ja DASH laitettu toimimaan node-media-serveriin

--- utils/config.js
+++ utils/config.js
@@ -7,19 +7,19 @@ let PORT = process.env.PORT
 let MEDIACONF = {
     logtype: 3,
     rtmp: {
-      port: process.env.RTMP_PORT,
-      chunk_size: process.env.CHUNK_SIZE,
-      gop_cache: process.env.GOP_CACHE,
-      ping: process.env.PING,
-      ping_timeout: process.env.PING_TIME_OUT
+      port: Number(process.env.RTMP_PORT),
+      chunk_size: Number(process.env.CHUNK_SIZE),
+      gop_cache: Boolean(process.env.GOP_CACHE),
+      ping: Number(process.env.PING),
+      ping_timeout: Number(process.env.PING_TIMEOUT)
     },
     http: {
-      port: process.env.MEDIA_HTTP,
-      allow_origin: process.env.ALLOW_ORIGIN,
+      port: Number(process.env.MEDIA_HTTP),
+      allow_origin: Number(process.env.ALLOW_ORIGIN),
       mediaroot: './dist/media'
     },
     auth: {
-        api : process.env.ENABLED,
+        api : Boolean(process.env.ENABLED),
         api_user: process.env.USER,
         api_pass: process.env.PASSWD,
       },
